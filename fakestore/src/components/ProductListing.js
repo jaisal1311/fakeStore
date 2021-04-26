b51beac7535581f5a35c7e6a10dfeb9ab4af8590
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-04-26 17:49:52 +0530

    product componet added

--- fakestore/src/components/ProductListing.js
+++ fakestore/src/components/ProductListing.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useCallback, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import axios from "axios";
 
 const ProductPage = () => {
   const products = useSelector((state) => state.allProducts);
